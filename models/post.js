ff34cfb03df66617295321bf4c9f00ff74694fa6
Daniel Barkan <pilpelbarkan@gmail.com>
2017-09-15 19:17:13 +0300

    sign in and logout appear conditionally

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ var like = new Schema({
     author: {type: String, required: true}
 });
 
-var schema = new Schema({
+var post = new Schema({
     header: {type: String, required: true},
     author: {type: String, required: true},
     body: {type: String, required: true},
@@ -19,6 +19,6 @@ var schema = new Schema({
     likes: [like]
 });
 
-module.exports = mongoose.model('post', schema);
+module.exports = mongoose.model('post', post);
 // module.exports = mongoose.model('comment', comment);
 // module.exports = mongoose.model('like', like);
